5bf66f267e4ab42dcda2f25e230371321d42a3e2
Egor Gavrilov <eg@rehub.team>
2024-12-02 18:11:37 +0100

    made draco avoid compressing compressed files

--- backend/db/index.sql
+++ backend/db/index.sql
@@ -2,6 +2,9 @@
 DROP INDEX IF EXISTS buildings_geom_idx;
 CREATE INDEX buildings_geom_idx ON building USING GIST(geom);
 
+ALTER TABLE public.building
+ADD CONSTRAINT gml_id_unique UNIQUE (gml_id);
+
 -- Cluster the table using the index
 CLUSTER building USING buildings_geom_idx;
 
